6844bfd1a0a6fdf991d38ad61fc1a535a597ba47
i.a.grekhov <iagrekhov.fronted@gmail.com>
2025-06-28 20:00:38 +0500

    Add pug file

--- packages/node-express/pug/src/app.ts
+++ packages/node-express/pug/src/app.ts
@@ -1,10 +1,26 @@
+import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
-import express from "express";
-
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const pug = require("pug");
+const compiledFunction = pug.compileFile(
+  "packages/node-express/pug/views/compile.pug"
+);
+
+const renderPage = pug.renderFile("packages/node-express/pug/views/render.pug");
+
+app.get("/compile-pug", (req: any, res: { send: (arg0: any) => void }) => {
+  const html = compiledFunction();
+  res.send(html);
+});
+
+app.get("/render-pug", (req: any, res: { send: (arg0: any) => void }) => {
+  const html = renderPage;
+  res.send(html);
+});
 
 app.listen(PORT, () => {
   console.log("Server started by PORT ", process.env.PORT);
